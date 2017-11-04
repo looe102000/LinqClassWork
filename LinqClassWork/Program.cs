e7253c9cf51fd23802855be4dd47d6ca1f005f47
looe102000 <looe102000@gmail.com>
2017-11-04 13:51:19 +0800

    重構語法

--- LinqClassWork/Program.cs
+++ LinqClassWork/Program.cs
@@ -10,6 +10,99 @@ namespace LinqClassWork
     {
         private static void Main(string[] args)
         {
+
+            CWOne();
+            CWTwo();
+            CWThree();
+            CWFour();
+            CWFive();
+
+            Console.ReadLine();
+        }
+
+        private static void CWFive()
+        {
+            Person2[] data1 = {
+            new Person2() { ID = 1, Name = "code6421", Age = 10 },
+            new Person2() { ID = 2, Name = "mary", Age = 11 },
+            new Person2() { ID = 3, Name = "mark", Age = 12 } };
+
+            Console.WriteLine("Linq 取得 a 出現的次數 方法1");
+
+            var countA = data1.Select(a => a.Name.ToArray().Where(s => s == 'a').Count()).Sum();
+            Console.WriteLine(countA);
+            Console.WriteLine();
+
+            Console.WriteLine("Linq 取得 a 出現的次數 方法2");
+            //you want know how many 'a' in the collection
+            var countB = (from s in data1
+                          let name = s.Name
+                          where name.Contains('a')
+                          select (from c in name where c == 'a' select c).Count()).Sum();
+
+            Console.WriteLine(countB);
+            Console.WriteLine();
+        }
+
+        private static void CWFour()
+        {
+            //使用 linq 查出 含有 b 的元素
+            Console.WriteLine("使用 linq 查出 含有 b 的元素 ");
+
+            string[] data2 = { "aa", "t", "cccccdd", "cb", "db", "tb" };
+            var result3 = from s1 in data2 where s1.Contains("b") select s1;
+            foreach (var item in result3)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine();
+
+            //string 陣列找出符合的元素
+            Console.WriteLine("string 陣列找出符合的元素");
+
+            string[] infilter = { "bbbbb", "ccccc" };
+
+            var result4 = from s1 in data2 where infilter.Contains(s1) select s1;
+            foreach (var item in result4)
+            {
+                Console.WriteLine(item.ToString());
+            }
+            Console.WriteLine();
+        }
+
+        private static void CWThree()
+        {
+            int[] data = { 1, 3, 5, 7, 9 };
+            //IEnumerator 迭代子 ；底下實作內容與 for each 效果差不多
+
+            Console.WriteLine("IEnumerator 迭代子 ；底下實作內容與 for each 效果差不多 ");
+            IEnumerator iterator = data.GetEnumerator();
+
+            while (iterator.MoveNext())
+            {
+                Console.WriteLine(iterator.Current.ToString());
+            }
+            Console.WriteLine();
+        }
+
+        private static void CWTwo()
+        {
+            //Linq 取得 Windows 資料夾中 .exe 檔案
+
+            Console.WriteLine("Windows 資料夾中 .exe 檔案");
+            var result1 = from s1 in Directory.GetFiles(@"C:\\Windows")
+                          where s1.EndsWith(".exe")
+                          select s1;
+
+            foreach (var item in result1)
+            {
+                Console.WriteLine(item);
+            }
+            Console.WriteLine();
+        }
+
+        private static void CWOne()
+        {
             int[] data = { 1, 3, 5, 7, 9 };
 
             //原始版本
@@ -31,80 +124,6 @@ namespace LinqClassWork
                 Console.WriteLine(item);
             }
             Console.WriteLine();
-            //Console.ReadLine();
-
-            //Linq 取得 Windows 資料夾中 .exe 檔案
-
-            Console.WriteLine("Windows 資料夾中 .exe 檔案");
-            var result1 = from s1 in Directory.GetFiles(@"C:\\Windows")
-                          where s1.EndsWith(".exe")
-                          select s1;
-
-            foreach (var item in result1)
-            {
-                Console.WriteLine(item);
-            }
-            Console.WriteLine();
-            //Console.ReadLine();
-
-            //IEnumerator 迭代子 ；底下實作內容與 for each 效果差不多
-
-            Console.WriteLine("IEnumerator 迭代子 ；底下實作內容與 for each 效果差不多 ");
-            IEnumerator iterator = data.GetEnumerator();
-
-            while (iterator.MoveNext())
-            {
-                Console.WriteLine(iterator.Current.ToString());
-            }
-            Console.WriteLine();
-            //Console.ReadLine();
-
-            //使用 linq 查出 含有 b 的元素
-            Console.WriteLine("使用 linq 查出 含有 b 的元素 ");
-
-            string[] data2 = { "aa", "t", "cccccdd", "cb", "db", "tb" };
-            var result3 = from s1 in data2 where s1.Contains("b") select s1;
-            foreach (var item in result3)
-            {
-                Console.WriteLine(item.ToString());
-            }
-            Console.WriteLine();
-
-            //string 陣列找出符合的元素
-            Console.WriteLine("string 陣列找出符合的元素");
-
-            string[] infilter = { "bbbbb","ccccc"};
-
-            var result4 = from s1 in data2 where infilter.Contains(s1) select s1;
-            foreach (var item in result4)
-            {
-                Console.WriteLine(item.ToString());
-            }
-            Console.WriteLine();
-
-            Person2[] data1 = {
-            new Person2() { ID = 1, Name = "code6421", Age = 10 },
-            new Person2() { ID = 2, Name = "mary", Age = 11 },
-            new Person2() { ID = 3, Name = "mark", Age = 12 } };
-
-            Console.WriteLine("Linq 取得 a 出現的次數 方法1");
-
-            var countA = data1.Select(a => a.Name.ToArray().Where(s => s == 'a').Count()).Sum();
-            Console.WriteLine(countA);
-            Console.WriteLine();
-
-            Console.WriteLine("Linq 取得 a 出現的次數 方法2");
-            //you want know how many 'a' in the collection
-            var countB = (from s in data1
-                          let name = s.Name
-                          where name.Contains('a')
-            select (from c in name where c == 'a' select c).Count()).Sum();
-
-            Console.WriteLine(countB);
-            Console.WriteLine();
-
-            Console.WriteLine();
-            Console.ReadLine();
         }
 
         //原始版本
